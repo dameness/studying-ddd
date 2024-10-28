6b896208bf1ad7719152359643f233d8c0003873
bernardo damiani <bmdamiani17@gmail.com>
2024-10-28 18:42:14 -0300

    feat: add question and answer attachment classes and create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
 import { CreateQuestionUseCase } from './create-question';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: CreateQuestionUseCase;
@@ -15,6 +16,7 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'title',
       content: 'New answer',
+      attachmentsIds: ['1', '2'],
     });
 
     expect(result.isRight()).toBe(true);
@@ -22,6 +24,11 @@ describe('Create Question', () => {
       expect(inMemoryQuestionsRepository.items[0]).toEqual(
         result.value.question
       );
+
+      expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+        expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+        expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+      ]);
     }
   });
 });
